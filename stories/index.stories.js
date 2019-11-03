ddb5a8b0b91ba99592f241185cf22645cb8d4e7c
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-03 23:13:37 +0330

    Fixed pastel story

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
-import InlineDemo from "../lib/PastelDemo";
+import PastelDemo from "../lib/PastelDemo";
 
 storiesOf("Demos", module)
     .add("Pastel Demo", () => <PastelDemo />)
