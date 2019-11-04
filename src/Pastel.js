8585f932fd01855a5a03f7c4d0d26c5870c815e5
Nikan Dalvand <nikandalvand@gmail.com>
2019-11-04 23:35:17 +0330

    Added more demo and single pastel support

--- src/Pastel.js
+++ src/Pastel.js
@@ -37,58 +37,55 @@ const styles = {
     chip: {
         margin: 1,
         color: '#000000',
-        '&.style-1': {
+        '&.style-1,&.style-green': {
             backgroundColor: green['A100'],
         },
-        '&.style-2': {
+        '&.style-2,&.style-amber': {
             backgroundColor: amber['A100'],
         },
-        '&.style-3': {
+        '&.style-3,&.style-red': {
             backgroundColor: red['A100'],
         },
-        '&.style-4': {
+        '&.style-4,&.style-color': {
             backgroundColor: blue['A100'],
         },
-        '&.style-5': {
+        '&.style-5,&.style-color': {
             backgroundColor: orange['A100'],
         },
-        '&.style-6': {
+        '&.style-6,&.style-color': {
             backgroundColor: lime['A100'],
         },
-        '&.style-7': {
+        '&.style-7,&.style-color': {
             backgroundColor: purple['A100'],
         },
-        '&.style-8': {
+        '&.style-8,&.style-color': {
             backgroundColor: teal['A100'],
         },
-        '&.style-9': {
+        '&.style-9,&.style-color': {
             backgroundColor: pink['A100'],
         },
-        '&.style-9': {
+        '&.style-9,&.style-color': {
             backgroundColor: indigo['A100'],
         },
-        '&.style-10': {
+        '&.style-10,&.style-color': {
             backgroundColor: deepPurple['A100'],
         },
-        '&.style-11': {
+        '&.style-11,&.style-color': {
             backgroundColor: deepOrange['A100'],
         },
-        '&.style-12': {
+        '&.style-12,&.style-color': {
             backgroundColor: cyan['A100'],
         },
-        '&.style-13': {
+        '&.style-13,&.style-color': {
             backgroundColor: lightBlue['A100'],
         },
-        '&.style-14': {
-            backgroundColor: lightBlue['A100'],
-        },
-        '&.style-15': {
+        '&.style-14,&.style-yellow': {
             backgroundColor: yellow['A100'],
         },
-        '&.style-16': {
+        '&.style-15,&.style-lightGreen': {
             backgroundColor: lightGreen['A100'],
         },
-        '&.style-17': {
+        '&.style-16,&.style-default': {
         },
     }
 }
@@ -100,20 +97,20 @@ class Pastel extends React.Component {
         this.rand = randomBetween(
             seededGenerator(props.label),
             1,
-            17
+            16
         )
     }
 
     render() {
         const props = this.props
         const classes = this.props.classes
+        const color = this.props.color || null
         return (
             <Chip 
-                color="inherit"
                 className={
-                    `${classes.chip} style-${this.rand}`
+                    `${classes.chip} style-${color === null ? this.rand : color}`
                 }
-                {...props} classes={null}
+                {...props} classes={null} color="default"
             />
         )
     }
--- src/PastelDemo.js
+++ src/PastelDemo.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import Pastel from './Pastel'
+import { Container, Card, CardContent, Typography } from '@material-ui/core'
 
 export default function PastelDemo() {
     return (
         <div>
-            <Pastel label="Hello!" />
-            <Pastel label="Goodbye!" />
-            <Pastel label="Small size chip" size="small" />
-            <Pastel label="Medium size chip" size="medium"/>
-            <Pastel label="Default size"/>
+            <Container>
+                <Card>
+                    <CardContent>
+                    <Typography variant="h4" gutterBottom>
+                            Auto colored pastels
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            The colors will stay the same for a specific label ex, "hello" will always result in the same color!
+                        </Typography>
+                        <Pastel label="Hello!" />
+                        <Pastel label="Goodbye!" />
+                        <Pastel label="Small size chip" size="small" />
+                        <Pastel label="Medium!" size="medium"/>
+                        <Pastel label="Default size"/>
+                        <Pastel label="Admin"/>
+                        <Pastel label="User"/>
+                        <Pastel label="Permission"/>
+                    </CardContent>
+                </Card>
+                <br />
+                <Card>
+                    <CardContent>
+                    <Typography variant="h4" gutterBottom>
+                            One colored pastels
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            The colors will stay the same for a specific label ex, "hello" will always result in the same color!
+                        </Typography>
+                        <Pastel label="Hello!" color="red" />
+                        <Pastel label="Goodbye!" color="green" />
+                        <Pastel label="Small size chip" color="amber" size="small" />
+                    </CardContent>
+                </Card>
+            </Container>
         </div>
     )
 }
